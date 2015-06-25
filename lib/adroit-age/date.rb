8a20273b341d5e12e2be035265702bb370228fc9
Van Huy <vanphu.quanghuy@glpgs.com>
2015-06-25 21:30:31 +0900

    Reindent, update readme

--- lib/adroit-age/date.rb
+++ lib/adroit-age/date.rb
@@ -1,12 +1,12 @@
 class Date
 
-	def find_age
-		Adroit::Age.new.find_year self
-	end
+  def find_age
+    Adroit::Age.new.find_year self
+  end
 
-	def find_age_with_month year_cust = nil, month_cust = nil
-		Adroit::Age.new.find_with_month self, year_cust, month_cust
-	end
+  def find_age_with_month year_cust = nil, month_cust = nil
+    Adroit::Age.new.find_with_month self, year_cust, month_cust
+  end
 
   def find_age_with_month_to_a
     Adroit::Age.new.find_with_month_to_a self
